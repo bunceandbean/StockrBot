593c500e73fcfbd80ed5797b0771983b4f219970
Ben Lilley <benlilleydev@gmail.com>
2022-06-21 14:59:31 -0400

    Added visualization package with init file

---
+++ main.py
@@ -0,0 +1,8 @@
+import yfinance as yf
+from visualization.ticker import render_ticker
+
+#msft = yf.Ticker("MSFT");
+#print(msft.info["pegRatio"])
+
+render_ticker("MSFT")
+
---
+++ visualization/ticker.py
@@ -0,0 +1,4 @@
+import pyfiglet as fig
+
+def render_ticker(ticker):
+ print(fig.figlet_format(ticker, font = "slant")) 
