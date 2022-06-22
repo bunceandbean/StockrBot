21dd1676e336772b7d66e5ac22386ed0de6cd80a
Ben Lilley <benlilleydev@gmail.com>
2022-06-22 15:08:35 -0400

    Basic terminal UI

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import yfinance as yf
 from visualization.ticker import render_ticker
+from visualization.tools import bcolors
 
-msft = yf.Ticker("MSFT")
-render_ticker(msft)
+def main():
+    tick_input = str(input(bcolors["OKGREEN"] + "Enter ticker symbol or write 'exit' to leave: " + bcolors["ENDC"])).lower()
+    if tick_input == 'exit':
+        return
+    tick = yf.Ticker(tick_input)
+    while len(tick.info) < 10:
+        tick_input = str(input(bcolors["WARNING"] + "Error: Invalid ticker symbol. Try again, or write 'exit' to leave: " + bcolors["ENDC"])).lower()
+        if tick_input == 'exit':
+            return
+        tick = yf.Ticker(tick_input)
+    render_ticker(tick)
+
+main()
--- visualization/ticker.py
+++ visualization/ticker.py
@@ -5,7 +5,10 @@ from visualization.tools import bcolors
 def render_ticker(ticker):
     try:
         print(bcolors["OKBLUE"] + fig.figlet_format(ticker.info["symbol"], font = "slant") + bcolors["ENDC"])
-        print(bcolors["OKGREEN"] + "Current Price: " + str(ticker.info["currentPrice"]) + bcolors["ENDC"])
+        try:
+            print(bcolors["OKGREEN"] + "Current Price: " + str(ticker.info["currentPrice"]) + bcolors["ENDC"])
+        except:
+            print(bcolors["WARNING"] + "Warning: Unknown or volatile data." + bcolors["ENDC"])
     except:
         print(bcolors["FAIL"] + "Error: Argument for render_ticker() was not a Ticker object." + bcolors["ENDC"])
         sys.exit()
